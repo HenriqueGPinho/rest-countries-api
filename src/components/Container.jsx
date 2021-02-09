d2ba35d83c3fd5a2d862cfb31cd91e43341e5897
Henrique Pinho <henrique.de.nf@gmail.com>
2021-02-09 12:48:38 -0300

    using API

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
 import styled from "styled-components";
 
@@ -13,73 +13,14 @@ const GridWrapper = styled.div`
   }
 `
 
-const countries = [
-  {
-    name: "Afghanistan",
-    population: 27657145,
-    region: "Asia",
-    capital: "Kabul",
-    flag: "https://restcountries.eu/data/afg.svg"
-  },
-  {
-    name: "Åland Islands",
-    population: 28875,
-    region: "Europe",
-    capital: "Mariehamn",
-    flag: "https://restcountries.eu/data/ala.svg"
-  },
-  {
-    name: "Albania",
-    population: 2886026,
-    region: "Europe",
-    capital: "Tirana",
-    flag: "https://restcountries.eu/data/alb.svg"
-  },
-  {
-    name: "Algeria",
-    population: 40400000,
-    region:  "Africa",
-    capital: "Algiers",
-    flag: "https://restcountries.eu/data/dza.svg"
-  },
-  {
-    name: "American Samoa",
-    population: 57100,
-    region: "Oceania",
-    capital: "Pago Pago",
-    flag: "https://restcountries.eu/data/asm.svg"
-  },
-  {
-    name: "Andorra",
-    population: 78014,
-    region: "Europe",
-    capital: "Andorra la Vella",
-    flag: "https://restcountries.eu/data/and.svg"
-  },
-  {
-    name: "Angola",
-    population: 25868000,
-    region: "Africa",
-    capital: "Luanda",
-    flag: "https://restcountries.eu/data/ago.svg"
-  },
-  {
-    name: "Anguilla",
-    population: 13452,
-    region: "Americas",
-    capital: "The Valley",
-    flag: "https://restcountries.eu/data/aia.svg"
-  },
-  {
-    name: "Antarctica",
-    population: 1000,
-    region: "Polar",
-    capital: "",
-    flag: "https://restcountries.eu/data/ata.svg"
-  }
-]
-
 function Container() {
+  const [countries, setCountries] = useState([]);
+
+  fetch("https://restcountries.eu/rest/v2/all")
+  .then(res => res.json())
+  .then(result => setCountries(result)
+  )
+
   return (
     <GridWrapper>
       {
