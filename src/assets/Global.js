3d8ed5a0e157acf89d71df46c7a8e997df4a540d
Henrique Pinho <henrique.de.nf@gmail.com>
2021-02-09 13:31:00 -0300

    fixed text color on dark mode change

--- src/assets/Global.js
+++ src/assets/Global.js
@@ -7,7 +7,7 @@ export const GlobalStyles = createGlobalStyle`
 
   body {
     background: ${({ theme }) => theme.background};
-    text: ${({ theme }) => theme.text};
+    color: ${({ theme }) => theme.text};
     font-family: 'Nunito Sans', sans-serif;
     /*transition: all 0.50s linear*/;
   }
