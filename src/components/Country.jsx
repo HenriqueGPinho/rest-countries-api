73d38e893cc40a340c09f12eba59ae18cdcac708
Henrique Pinho <henrique.de.nf@gmail.com>
2021-02-19 10:43:29 -0300

    searchParams

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 function Country(props) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get('id');
+
+  console.log(id);
+
   return (
     //TODO create the page
     <h1>Paeis</h1>
